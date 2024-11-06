14acbf11fc24f158313759be7b7d42be3afe2478
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2024-11-06 22:50:23 +0600

    fix the file upload problem

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,22 @@
 
 const express = require('express');
 const multer = require('multer');
+const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 3000;
 
 
+
+const uploadDir = 'uploads';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); 
+    cb(null, uploadDir); 
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname)); 
@@ -22,7 +30,7 @@ const upload = multer({
 
 
 app.post('/api/upload', upload, (req, res) => {
- 
+  console.log(req.files);
   if (!req.files) {
     return res.status(400).send('No files were uploaded.');
   }
@@ -37,7 +45,7 @@ app.post('/api/upload', upload, (req, res) => {
   });
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
